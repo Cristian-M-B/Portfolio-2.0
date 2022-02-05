a9c782e71e5164ec1e1ec6b9a3705a5ddab064f3
Cristian-M-B <crisbaronetto@hotmail.com>
2022-02-04 21:39:40 -0300

    add pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Home from './pages/Home.js';
+import Technologies from './pages/Technologies.js';
+import Projects from './pages/Projects.js';
 import './App.css';
 
 export default function App() {
   return (
-    <div>
-      APP
-    </div>
+    <>
+      <Home />
+      <Technologies />
+      <Projects />
+    </>
   )
 }
