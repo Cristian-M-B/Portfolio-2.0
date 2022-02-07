ed9c18bad8b9a6135f56751593cff9e379b48a5d
Cristian-M-B <crisbaronetto@hotmail.com>
2022-02-07 11:27:07 -0300

    add info

---
+++ src/data/technologies.js
@@ -0,0 +1,48 @@
+const technologies = [
+    {
+        name: 'HTML',
+        image: 'https://raw.githubusercontent.com/yurijserrano/Github-Profile-Readme-Logos/master/others/html.svg'
+    },
+    {
+        name: 'CSS',
+        image: 'https://raw.githubusercontent.com/yurijserrano/Github-Profile-Readme-Logos/master/others/css.svg'
+    },
+    {
+        name: 'Sass',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/245px-Sass_Logo_Color.svg.png'
+    },
+    {
+        name: 'Material UI',
+        image: 'https://camo.githubusercontent.com/4915c672a5aa2a0bb15d4ece78217ef9222c2cd99095841d0fd48abb55e1f8af/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f642f64632f4c6f676f5f6d6174657269616c5f64657369676e2e7376672f35313270782d4c6f676f5f6d6174657269616c5f64657369676e2e7376672e706e67'
+    },
+    {
+        name: 'JavaScript',
+        image: 'https://raw.githubusercontent.com/yurijserrano/Github-Profile-Readme-Logos/master/programming%20languages/javascript.svg'
+    },
+    {
+        name: 'React',
+        image: 'https://raw.githubusercontent.com/yurijserrano/Github-Profile-Readme-Logos/master/frameworks/react.svg'
+    },
+    {
+        name: 'Redux',
+        image: 'https://raw.githubusercontent.com/yurijserrano/Github-Profile-Readme-Logos/master/frameworks/redux.svg'
+    },
+    {
+        name: 'Node',
+        image: 'https://raw.githubusercontent.com/yurijserrano/Github-Profile-Readme-Logos/master/frameworks/nodejs.svg'
+    },
+    {
+        name: 'Express',
+        image: 'https://camo.githubusercontent.com/28e93a1bfe79f991ddcd35f7833e8537f0e7b31aa326dfbe98fe7eb538b40b46/68747470733a2f2f63646e2e69636f6e2d69636f6e732e636f6d2f69636f6e73322f323431352f504e472f3531322f657870726573735f6f726967696e616c5f776f72646d61726b5f6c6f676f5f69636f6e5f3134363532382e706e67'
+    },
+    {
+        name: 'Sequelize',
+        image: 'https://camo.githubusercontent.com/c7df0ed52a480ff725aac7ac3a11c8aedb6f60ea8ab01929c6adea9903589222/68747470733a2f2f63646e2e69636f6e2d69636f6e732e636f6d2f69636f6e73322f323130372f504e472f3531322f66696c655f747970655f73657175656c697a655f69636f6e5f3133303137332e706e67'
+    },
+    {
+        name: 'PostgreSQL',
+        image: 'https://raw.githubusercontent.com/yurijserrano/Github-Profile-Readme-Logos/master/databases/postgresql.svg'
+    }
+]
+
+export default technologies;
