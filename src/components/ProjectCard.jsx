d2f56353ec61e0378c29d6e1fa0cd324c1e6af77
Cristian-M-B <crisbaronetto@hotmail.com>
2022-02-09 18:31:39 -0300

    component ProjectCard

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Grid, Card, CardContent, CardActions, IconButton, Collapse, Typography } from '@material-ui/core';
+import { Link, ExpandMore, ExpandLess } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/styles';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
+Aos.init();
+
+const useStyles = makeStyles((theme) => ({
+    card: {
+        width: '40vw',
+    },
+    '@media (max-width: 600px)': {
+        card: {
+            width: '80vw',
+        }
+    }
+}));
+
+export default function ProjectCard({ name, image, link, description }) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Grid item data-aos='flip-right' style={{ margin: '2vh' }}>
+            <Card className={classes.card}>
+                <CardContent>
+                    <Typography align='center' variant='h6'>
+                        {name}
+                    </Typography>
+                </CardContent>
+                <img src={image} alt={name} height='200vh' width='100%' />
+                <CardActions style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <a href={link} target='_blank' rel='noreferrer'>
+                        <IconButton>
+                            <Link />
+                        </IconButton>
+                    </a>
+                    <IconButton onClick={handleExpandClick}>
+                        {!expanded
+                            ? <ExpandMore />
+                            : <ExpandLess />
+                        }
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout='auto' unmountOnExit>
+                    <CardContent>
+                        <Typography>
+                            {description}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        </Grid>
+    )
+}
