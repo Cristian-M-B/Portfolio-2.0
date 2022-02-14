080be8f1e43c548ff9a574330b1acbdaaf149539
Cristian-M-B <crisbaronetto@hotmail.com>
2022-02-14 12:51:58 -0300

    component Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { animateScroll } from 'react-scroll';
+import { Grid, IconButton } from '@material-ui/core';
+import { ArrowUpward } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/styles';
+import '../styles/animations.css';
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+        width: '100%',
+        height: '8vh',
+        color: 'white',
+        fontWeight: 'bold',
+        paddingTop: '40px',
+    },
+    icon: {
+        color: 'white',
+        '&:hover': {
+            backgroundColor: 'transparent',
+        },
+    },
+}));
+
+export default function Footer() {
+    const classes = useStyles();
+
+    const scrollToTop = () => {
+        animateScroll.scrollToTop();
+    }
+
+    return (
+        <Grid
+            container
+            direction='row'
+            justifyContent='center'
+            alignItems='center'
+            className={classes.footer}
+        >
+            <IconButton onClick={scrollToTop} className={classes.icon}>
+                <ArrowUpward fontSize='large' className='scroll' />
+            </IconButton>
+        </Grid>
+    )
+}
