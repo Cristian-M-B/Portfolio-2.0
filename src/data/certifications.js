388a9d96b47ba023fcbea783e823f0f310b7a1b6
Cristian-M-B <crisbaronetto@hotmail.com>
2022-03-03 16:38:27 -0300

    add certifications info

---
+++ src/data/certifications.js
@@ -0,0 +1,20 @@
+import CertificadoFullStackWebDeveloper from '../assets/images/certifications/CertificadoFullStackWebDeveloper.png';
+import CertificadoJavaScriptBasico from '../assets/images/certifications/CertificadoJavaScriptBasico.png';
+import CertificadoJavaScriptIntermedio from '../assets/images/certifications/CertificadoJavaScriptIntermedio.png';
+
+const certifications = [
+    {
+        name: 'Full Stack Web Developer',
+        image: CertificadoFullStackWebDeveloper,
+    },
+    {
+        name: 'JavaScript (Básico)',
+        image: CertificadoJavaScriptBasico,
+    },
+    {
+        name:'JavaScript (Intermedio)',
+        image: CertificadoJavaScriptIntermedio,
+    }
+]
+
+export default certifications;
