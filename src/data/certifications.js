505523af8e8e2af69eee418ca0eb0fabe74f2eeb
Cristian-M-B <crisbaronetto@hotmail.com>
2022-03-08 16:52:35 -0300

    add info

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,19 +1,28 @@
 import CertificadoFullStackWebDeveloper from '../assets/images/certifications/CertificadoFullStackWebDeveloper.png';
 import CertificadoJavaScriptBasico from '../assets/images/certifications/CertificadoJavaScriptBasico.png';
 import CertificadoJavaScriptIntermedio from '../assets/images/certifications/CertificadoJavaScriptIntermedio.png';
+import CertificadoReactBasico from '../assets/images/certifications/CertificadoReactBasico.png';
 
 const certifications = [
     {
         name: 'Full Stack Web Developer',
         image: CertificadoFullStackWebDeveloper,
+        link: 'https://certificates.soyhenry.com/cert?id=69fcab18-ec7e-431a-a06c-d2de58ec9c1e',
     },
     {
         name: 'JavaScript (Básico)',
         image: CertificadoJavaScriptBasico,
+        link: 'https://www.hackerrank.com/certificates/feac2ed2be80',
     },
     {
         name:'JavaScript (Intermedio)',
         image: CertificadoJavaScriptIntermedio,
+        link: 'https://www.hackerrank.com/certificates/43284c6eecd3',
+    },
+    {
+        name:'React (Básico)',
+        image: CertificadoReactBasico,
+        link: 'https://www.hackerrank.com/certificates/570e7b711726',
     }
 ]
 
