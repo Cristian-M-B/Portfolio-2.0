d05fbeb5781811be0cb6048c7c00d4690f190090
Cristian-M-B <crisbaronetto@hotmail.com>
2022-08-29 10:02:01 -0300

    change the certificate link next

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -2,39 +2,39 @@ import CertificadoFullStackWebDeveloper from '../assets/images/certifications/Ce
 import CertificadoJavaScriptBasico from '../assets/images/certifications/CertificadoJavaScriptBasico.png';
 import CertificadoJavaScriptIntermedio from '../assets/images/certifications/CertificadoJavaScriptIntermedio.png';
 import CertificadoReactBasico from '../assets/images/certifications/CertificadoReactBasico.png';
-import CertificadoNext from '../assets/images/certifications/CertificadoNext.png';
 import CertificadoTypeScript from '../assets/images/certifications/CertificadoTypeScript.png';
+import CertificadoNext from '../assets/images/certifications/CertificadoNext.png';
 
 const certifications = [
     {
         name: 'Full Stack Web Developer',
         image: CertificadoFullStackWebDeveloper,
-        link: 'https://certificates.soyhenry.com/cert?id=69fcab18-ec7e-431a-a06c-d2de58ec9c1e',
+        link: 'https://certificates.soyhenry.com/cert?id=69fcab18-ec7e-431a-a06c-d2de58ec9c1e'
     },
     {
         name: 'JavaScript (Básico)',
         image: CertificadoJavaScriptBasico,
-        link: 'https://www.hackerrank.com/certificates/feac2ed2be80',
+        link: 'https://www.hackerrank.com/certificates/feac2ed2be80'
     },
     {
-        name:'JavaScript (Intermedio)',
+        name: 'JavaScript (Intermedio)',
         image: CertificadoJavaScriptIntermedio,
-        link: 'https://www.hackerrank.com/certificates/43284c6eecd3',
+        link: 'https://www.hackerrank.com/certificates/43284c6eecd3'
     },
     {
-        name:'React (Básico)',
+        name: 'React (Básico)',
         image: CertificadoReactBasico,
-        link: 'https://www.hackerrank.com/certificates/570e7b711726',
+        link: 'https://www.hackerrank.com/certificates/570e7b711726'
     },
     {
-        name:'Next',
-        image: CertificadoNext,
-        link: 'https://codigofacilito.com/certificates/42074.pdf',
-    },
-    {
-        name:'TypeScript',
+        name: 'TypeScript',
         image: CertificadoTypeScript,
         link: 'https://codigofacilito.com/certificates/42454.pdf'
+    },
+    {
+        name: 'Next',
+        image: CertificadoNext,
+        link: 'https://codigofacilito.com/certificates/48149.pdf'
     }
 ]
 
