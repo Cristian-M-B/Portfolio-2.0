5f63deca5f7fa7ffa8ebed8ce9036f056de1723a
Cristian-M-B <crisbaronetto@hotmail.com>
2023-05-24 15:29:05 -0300

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,21 +6,21 @@ const projects = [
     {
         name: 'TecnoCommerce',
         image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHH5iLhHb9gbEJgU3su2lsQ3Y4qsixiHpsHQ&usqp=CAU',
-        description: 'E-commerce de electrodomésticos y tecnología. La app consta con tres niveles de ingreso (invitado, usuario y administrador), cada uno con funcionalidades adicionales. Por ejemplo el usuario puede seleccionar productos como favoritos, ver y modificar su perfil y dejar una calificación del producto que compro. Mientras que el administrador además puede gestionar los productos, las ordenes de compras y los usuarios registrados. La app incluye pasarela de pago mediante Mercado Pago.',
+        description: 'E-commerce de electrodomésticos y tecnología. La app consta con tres niveles de ingreso (invitado, usuario y administrador), cada una con sus funcionalidades características. Por ejemplo el usuario puede seleccionar productos como favoritos, ver y modificar su perfil y dejar una calificación del producto que compro. Mientras que el administrador puede gestionar los productos, las ordenes de compras y los usuarios registrados. La app incluye pasarela de pago mediante Mercado Pago.',
         link: 'https://tecnocommerce.vercel.app'
     },
-    {
-        name: 'AmadeuS',
-        image: amadeus,
-        description: 'E-commerce con temática de instrumentos musicales. La app consta con tres niveles de ingreso (invitado, usuario y administrador), cada uno con funcionalidades adicionales. Por ejemplo el usuario puede seleccionar productos como favoritos, ver y modificar su perfil y dejar una calificación del instrumento que compro. Mientras que el administrador además puede gestionar los productos, las ordenes de compras y los usuarios registrados. La app incluye autentificación mediante Auth0 y pasarela de pago mediante Mercado Pago.',
-        link: 'https://amadeus.vercel.app'
-    },
-    {
-        name: 'El Chef',
-        image: elChef,
-        description: 'Single Page Application (SPA) sobre recetas de cocina que trae información desde una API externa (Spoonacular) y permite la creación de nuevas recetas que se guardan en una base de datos local. Cuenta con visualización de las recetas y sus respectivos detalles, búsqueda por nombre, filtrado por tipo de dieta, ordenamiento por nombre y puntuación, paginado y un formulario controlado para la validación de los datos al crear una nueva receta.',
-        link: 'https://elchef.vercel.app'
-    },
+    // {
+    //     name: 'AmadeuS',
+    //     image: amadeus,
+    //     description: 'E-commerce con temática de instrumentos musicales. La app consta con tres niveles de ingreso (invitado, usuario y administrador), cada uno con funcionalidades adicionales. Por ejemplo el usuario puede seleccionar productos como favoritos, ver y modificar su perfil y dejar una calificación del instrumento que compro. Mientras que el administrador además puede gestionar los productos, las ordenes de compras y los usuarios registrados. La app incluye autentificación mediante Auth0 y pasarela de pago mediante Mercado Pago.',
+    //     link: 'https://amadeus.vercel.app'
+    // },
+    // {
+    //     name: 'El Chef',
+    //     image: elChef,
+    //     description: 'Single Page Application (SPA) sobre recetas de cocina que trae información desde una API externa (Spoonacular) y permite la creación de nuevas recetas que se guardan en una base de datos local. Cuenta con visualización de las recetas y sus respectivos detalles, búsqueda por nombre, filtrado por tipo de dieta, ordenamiento por nombre y puntuación, paginado y un formulario controlado para la validación de los datos al crear una nueva receta.',
+    //     link: 'https://elchef.vercel.app'
+    // },
     {
         name: 'PokeMania',
         image: pokeMania,
